4839fceda4c7f15d3fbef38a85f224a832d79cef
Nasi <51403097+Nasisolo@users.noreply.github.com>
2022-11-06 16:34:52 +0100

    Fix in Main: removedc extra print

--- src/com/nasi/correzioneStackEPila/Main.java
+++ src/com/nasi/correzioneStackEPila/Main.java
@@ -3,18 +3,18 @@ package com.nasi.correzioneStackEPila;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-	Stack s = new Stack(8);
-	s.push(1);
-	s.push(2);
-	s.push(3);
-	s.push(4);
-	s.push(5);
-	s.push(9);
-	System.out.println(s.size());
+		Stack s = new Stack(8);
+		s.push(1);
+		s.push(2);
+		s.push(3);
+		s.push(4);
+		s.push(5);
+		s.push(9);
+		System.out.println(s.size());
 		System.out.printf("2. Pop elements from stack : ");
 		while (!s.isEmpty()) {
 			System.out.printf(" %d", s.pop());
 		}
-		System.out.println(s.top());
+		//System.out.println(s.top());
     }
 }
